ee441248706f01764235e30c921ae211aecf7218
Kai Hernandez <Kai@Kais-MBP.wv.cc.cmu.edu>
2016-09-17 14:55:47 -0400

    added to url section

--- juniorhack/firstTest/urls.py
+++ juniorhack/firstTest/urls.py
@@ -5,11 +5,17 @@ from . import views
 urlpatterns = [
     url(r'^i$', views.index, name='index'),
     url(r'^name$', views.get_name, name='name'),
+    url(r'^update$', views.updateArray, name='update array')
+    url(r'^result$', views.mapResult, name='mapResult')
+    url(r'^finalResult$', views.res, name='finalResult')
 ]
 
 #from django.conf.urls import url
 
 #from firstTest import views
+
+updateArray
+results
 
 #urlpatterns = [
 #	url(r'^$', views.index, name='index'),
--- juniorhack/firstTest/views.py
+++ juniorhack/firstTest/views.py
@@ -2,15 +2,17 @@
 
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .form import PersonForm
+from .models import *
 
 def get_name(request):
 	if request.method == 'POST':
-		form1 = PersonForm(request.POST)
+		form1 = PersonForm(request.form)
 		if form1.is_valid():
+			return 
 			return HttpResponse('/thanks/')
 	else:
 		form1 = PersonForm()
@@ -20,3 +22,5 @@ def get_name(request):
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the polls index.")
+
+def trial(request, question_id):

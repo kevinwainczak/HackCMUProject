c94748d50baed61a54d92c1f8d61907d66d44bf5
Kevin Wainczak <kevinwainczak@Kevins-MacBook-Pro-6.local>
2016-09-17 01:04:07 -0400

    changing models

--- juniorhack/firstTest/models.py
+++ juniorhack/firstTest/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+@python_2_unicode_compatible
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    age = models.IntegerField()
+    race = models.CharField(max_length=255)
+    sexuality = models.CharField(max_length=8)
+    roast = models.CharField(max_length=255)
+
+
+@python_2_unicode_compatible
+class People(models.Model):
+    person1 = models.ForeignKey(Person)
+    person2 = models.ForeignKey(Person)
+    person3 = models.ForeignKey(Person)
+    person4 = models.ForeignKey(Person)
+
+@python_2_unicode_compatible
+class OpenConversations(models.Model):
+    conversationTopic = models.CharField(max_length=200)
+    people = models.ForeignKey(People)

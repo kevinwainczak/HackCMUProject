d20f79c317ad7ca250bf98986bcc2161ccbdd010
Kai Hernandez <Kai@Kais-MBP.wv.cc.cmu.edu>
2016-09-17 14:40:49 -0400

    form with submit working

--- juniorhack/firstTest/form.py
+++ juniorhack/firstTest/form.py
@@ -1,11 +1,14 @@
 from django import forms
 
-race_choices = [('Asian','asian'), ('White', 'Black')]
+race_choices = [('asian','Asian'), ('white', 'White'), ('black', 'Black')]
+gender_choices = [('straight', 'Straight'), ('gay', 'Gay'), ('bi', 'Bi')]
+
 
 class PersonForm (forms.Form):
 	your_name = forms.CharField(label='Your name', max_length=100)
 	your_age = forms.IntegerField(label='Your age')
 	your_race = forms.ChoiceField(label='Your race', widget=forms.Select, choices=race_choices,)
+	your_genger = forms.ChoiceField(label='Gender', widget=forms.Select, choices=gender_choices,)
 
 
 

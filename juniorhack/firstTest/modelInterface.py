8af58c8bac9fc3b27af48324a83c2f50fbbc98ba
Kevin Wainczak <kevinwainczak@Kevins-MacBook-Pro-6.local>
2016-09-17 11:24:13 -0400

    Models added and interface begun

---
+++ juniorhack/firstTest/modelInterface.py
@@ -0,0 +1,13 @@
+from firstTest.models import *
+
+def newConversation():
+    return 0
+
+def findConversations():
+    return 0
+
+def addPersonToConvo():
+    return 0
+
+def removeConversation():
+    return 0

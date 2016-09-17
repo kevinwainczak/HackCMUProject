1cbcedc0143939706a1de6b6b2e8baacafa25295
Kai Hernandez <Kai@Kais-MBP.wv.cc.cmu.edu>
2016-09-17 11:19:13 -0400

    got urls working

--- juniorhack/juniorhack/urls.py
+++ juniorhack/juniorhack/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^polls/', include('firstTest.url')),
+	url(r'^', include('firstTest.urls')),
     url(r'^admin/', admin.site.urls),
 ]

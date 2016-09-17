ab8a0eeb2c22e9cdbb3cc4b457db6fe3c233c57c
Kai Hernandez <Kai@Kais-MBP.wv.cc.cmu.edu>
2016-09-17 18:32:45 -0400

    sexuality fixed

--- juniorhack/firstTest/form.py
+++ juniorhack/firstTest/form.py
@@ -1,18 +1,18 @@
 from django import forms
 
-race_choices = [('asian','Asian'), ('white', 'White'), ('black', 'Black'), ('indian', 'Indian'),
+race_choices = [('asian','Asian'), ('white', 'White'), ('black', 'Black'), ('indian', 'Latino'),
 				('native american', 'Native American')]
 sexuality_choices = [('straight', 'Straight'), ('gay', 'Gay'), ('bi', 'Bi')]
 gender_choices = [('male', 'Male'), ('female', 'Female'), ('trans', 'Transgender')]
 roast_choices = [('light', 'Light'), ('medium', 'Medium'), ('dark', 'Dark')]
-topic_choices = [('politics', 'Politics'), ('environment', 'Environment'), ('kai', 'Kai'), ('religion', 'Religion')]
+topic_choices = [('politics', 'Politics'), ('environment', 'Environment'), ('religion', 'Religion')]
 
 class PersonForm (forms.Form):
 	your_name = forms.CharField(label='Your Name', max_length=100)
 	your_age = forms.IntegerField(label='Age')
 	your_race = forms.ChoiceField(label='Race', widget=forms.RadioSelect, choices=race_choices,)
 	your_gender = forms.ChoiceField(label='Gender', widget=forms.RadioSelect, choices=gender_choices,)
-	your_sexuality = forms.CharField(label='Sexuality')# widget)
+	your_sexuality = forms.ChoiceField(label='Sexuality', widget=forms.RadioSelect, choices=sexuality_choices)# widget)
 	your_roast = forms.ChoiceField(label='Your Preferred Roast', widget=forms.RadioSelect, choices=roast_choices)
 	your_topic = forms.ChoiceField(label='Topic you Want to Talk About', widget=forms.RadioSelect, choices=topic_choices)
 
